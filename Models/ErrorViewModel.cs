bf1475de3bba55815973a0fd8bc77b16edc29276
ismailusta <ismail.usta028@gmail.com>
2025-06-03 17:45:23 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DynamicCv.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
