2673b37d8818479968dbc9364e909b6da667012c
ismailusta <ismail.usta028@gmail.com>
2025-06-10 11:56:34 +0300

    sadece login işlemi kaldı

--- Controllers/SocialMediaController.cs
+++ Controllers/SocialMediaController.cs
@@ -12,5 +12,38 @@ namespace DynamicCv.Controllers
             var degerler = repo.List();
             return View(degerler);
         }
+        public IActionResult DeleteSocialMedia(int id)
+        {
+            TblSocialMedia t = repo.Find(x=>x.Id == id);
+            repo.TRemove(t);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult AddSocialMedia() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddSocialMedia(TblSocialMedia p) 
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditSocialMedia(int id) 
+        {
+            TblSocialMedia t = repo.Find(x => x.Id == id);
+            return View(t);
+        }
+        [HttpPost]
+        public IActionResult EditSocialMedia(TblSocialMedia p)
+        {
+            TblSocialMedia t = repo.Find(x =>x.Id == p.Id);
+            t.AppName = p.AppName;
+            t.Icon= p.Icon;
+            t.Link= p.Link;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
+        }
     }
 }
--- obj/Debug/net8.0/DynamicCv.AssemblyInfo.cs
+++ obj/Debug/net8.0/DynamicCv.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DynamicCv")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e825778a91251d6ddf6ac234f19bdf9fe6e885b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7802e1773423bf911baf8e3f474119b64fffb544")]
 [assembly: System.Reflection.AssemblyProductAttribute("DynamicCv")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DynamicCv")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
